2d752e6e29da5e517aac0093197490542e8fd9a8
yuiiton <151460200+yuiiton@users.noreply.github.com>
2025-10-06 22:12:13 -0300

    aaaaaa bugs e bugs

--- suporte/script.js
+++ suporte/script.js
@@ -105,9 +105,9 @@ if (openComandos && floatComandos && closeComandos) {
 }
 
 /* Links para páginas separadas */
-if (btnCMD) btnCMD.addEventListener("click", () => { window.location.href = "TIME-TO-OPTIMIZE/windows/cmd.html"; });
-if (btnPowerShell) btnPowerShell.addEventListener("click", () => { window.location.href = "TIME-TO-OPTIMIZE/windows/powershell.html"; });
-if (btnWinR) btnWinR.addEventListener("click", () => { window.location.href = "TIME-TO-OPTIMIZE/windows/winr.html"; });
+if (btnCMD) btnCMD.addEventListener("click", () => { window.location.href = "/windows/cmd.html"; });
+if (btnPowerShell) btnPowerShell.addEventListener("click", () => { window.location.href = "/windows/powershell.html"; });
+if (btnWinR) btnWinR.addEventListener("click", () => { window.location.href = "/windows/winr.html"; });
 
 /* ===== Backup e Salvamento em Nuvem ===== */
 const openBackup = document.getElementById("openBackup");
