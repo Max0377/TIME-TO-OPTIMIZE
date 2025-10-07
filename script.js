4a9daea01bd69c2d8ecd20354465989cbaeab454
yuiiton <151460200+yuiiton@users.noreply.github.com>
2025-10-06 21:11:05 -0300

    sla

--- script.js
+++ script.js
@@ -218,7 +218,7 @@ if (openComandos && floatComandos && closeComandos) {
 /* Links para páginas separadas */
 if (btnCMD) btnCMD.addEventListener("click", () => { window.location.href = "/TIME-TO-OPTIMIZE/windows/cmd.html"); });
 if (btnPowerShell) btnPowerShell.addEventListener("click", () => { window.location.href = "/TIME-TO-OPTIMIZE/windows/powershell.html"; });
-if (btnWinR) btnWinR.addEventListener("click", () => { window.location.href = "/TIME-TO-OPTIMIZE/windows/winr.html""; });
+if (btnWinR) btnWinR.addEventListener("click", () => { window.location.href = "/TIME-TO-OPTIMIZE/windows/winr.html"; });
 
 /* ===== Backup ===== */
 const openBackup = document.getElementById("openBackup");
